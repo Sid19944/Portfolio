a98c69dc08241364f6129015e0ad114cf5187e70
Siddharth Sarkar <sidsarkar2112@gmail.com>
2026-01-16 21:39:09 +0530

    routes

--- Dashboard/src/main.jsx
+++ Dashboard/src/main.jsx
@@ -5,9 +5,7 @@ import App from "./App.jsx";
 createRoot(document.getElementById("root")).render(
   <div className="min-h-screen bg-black text-white overflow-auto">
     <BrowserRouter>
-      <Routes>
-        <Route path="/*" element={<App />} />
-      </Routes>
+      <App />
     </BrowserRouter>
   </div>
 );
