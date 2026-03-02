c2679bde6b28d0a412964f348101754b49cbbe98
Siddharth Sarkar <sidsarkar2112@gmail.com>
2026-03-02 23:17:06 +0530

    production url in dashboard

--- Dashboard/src/Api.jsx
+++ Dashboard/src/Api.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-// const url = "https://portfolio-backend-xmod.onrender.com";
-const url = "http://localhost:3000"
+const url = "https://portfolio-backend-xmod.onrender.com";
+// const url = "http://localhost:3000"
 
 const userApi = axios.create({
   baseURL: `${url}/auth/v1/user`,
